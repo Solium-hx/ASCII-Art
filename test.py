486812a35dbadd52f0e17fa49e9305a73b86176f
Soliium <rockerchinayush@gmail.com>
2022-06-06 15:55:30 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import cv2
+from PIL import Image;
+
+im = Image.new(mode="L", size=(200, 200))
+im.show()
+
+
+
+img = cv2.imread('test1.jpg', 0)
+print(img)
+
+cv2.imwrite('test1_res.jpg', img)
+cv2.imshow('result', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
