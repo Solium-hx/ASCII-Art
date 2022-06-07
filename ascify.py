00115946b73e6a4d17792467bd26954ffab3bbda
Soliium <rockerchinayush@gmail.com>
2022-06-08 01:04:09 +0530

    Working Project

--- ascify.py
+++ ascify.py
@@ -69,20 +69,18 @@ for i in range(num_rows):
     min_h = min(int((i+1)*cell_h), height)
     row_pix = int(i*cell_h)
     line = "".join([char_list[min(int(np.mean(img[row_pix:min_h, int(j*cell_w):min(int((j+1)*cell_w), width)])/255*num_chars), num_chars-1)]
-                    for j in range(num_cols)]) + "\n"
+                    for j in range(num_cols)])
 
 # Draw string at a given position (x,y)
-    draw.text((0, i+char_height), line, fill = 255-bg_code, font = font)
+    draw.text((0, (i+char_height)*25), line, fill = 255-bg_code, font = font)
 
 # Inverting Image and removing excess borders
 
-# ********************** NOT CROPPED **********************
 if bg == "white":
-    final_img = ImageOps.invert(out_img)
-else:
-    final_img = out_img
-
-#Run to open a window of the final image
-final_img.show()
+    cropped_img = ImageOps.invert(out_img).getbbox()
+elif bg == "black":
+    cropped_img = out_img.getbbox()
 
 # ********************** SAVING IMAGE **********************
+out_img = out_img.crop(cropped_img)
+out_img.save("./output_images/2_output1.jpg")
