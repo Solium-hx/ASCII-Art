7c942f3570174e180c4c80ac3e8c47c4be8525e2
Soliium <rockerchinayush@gmail.com>
2022-06-18 19:16:39 +0530

    minor changes

--- ascify.py
+++ ascify.py
@@ -41,10 +41,15 @@ def convert(img_loc):
 
     for i in range(int(num_rows)):
         for j in range(int(num_cols)):
-            partial_image = image[int(i*cell_h):min(int((i+1)*cell_h), height), int(j*cell_w):min(int((j+1)*cell_w), width), :]
+            partial_image = image[  int(i*cell_h)   :   min(int((i+1)*cell_h), height),
+                                    int(j*cell_w)   :   min(int((j+1)*cell_w), width),
+                                                    :                                   ]
+
             partial_avg_color = np.sum(np.sum(partial_image, axis=0), axis=0)/(cell_h*cell_w)
             partial_avg_color = tuple(partial_avg_color.astype(np.int32).tolist())
+
             c = char_list[min(int((int(np.mean(partial_image))*num_chars)/255), num_chars-1)]
+            
             draw.text((j*char_width, i*char_height), c, fill = partial_avg_color, font = font)
 
     if bg == "white":
