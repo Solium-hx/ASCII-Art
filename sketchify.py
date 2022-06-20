560d7a26ea3bc4ea89fb9f9fa1b27aa46896a72d
Soliium <rockerchinayush@gmail.com>
2022-06-21 03:32:13 +0530

    Option of make pencil sketches

--- sketchify.py
+++ sketchify.py
@@ -7,6 +7,7 @@ def convert(img_loc):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     blur_gray_image = cv2.GaussianBlur(gray_image, (15, 15), 0)
+    
     sketch_image = cv2.divide(gray_image, blur_gray_image, scale=256.0)
 
     return sketch_image
